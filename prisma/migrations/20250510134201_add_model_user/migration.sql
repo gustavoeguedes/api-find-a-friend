c19dec9d63a9643368f5b7fc31ec0a496d8de260
gustavoeguedes <gustavoestevesguedes12@gmail.com>
2025-05-10 10:42:45 -0300

    feat: add user migration

---
+++ prisma/migrations/20250510134201_add_model_user/migration.sql
@@ -0,0 +1,17 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ORGANIZATION', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password_hash" TEXT NOT NULL,
+    "organizationId" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'ORGANIZATION',
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "users" ADD CONSTRAINT "users_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "organizations"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
